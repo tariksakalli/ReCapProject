8f497f43c071a9a835b5d77eeb76ce8413cf63ce
Tarık Sakallı <tarik.sakalli@outlook.com>
2021-03-18 00:25:14 +0300

    Day 12 Assignment 1

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,13 +21,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.DailyPrice <= 0)
-            {
-                return new ErrorResult(Messages.ProductPriceInvalid);
-            }
-
             _carDal.Add(car);
 
             return new SuccessResult(Messages.RecordAdded);
